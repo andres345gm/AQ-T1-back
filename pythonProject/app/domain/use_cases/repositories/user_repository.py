b4afae86a02e1c228d579d76b94291a828e8d5bf
andres345gm <andres345gm@gmail.com>
2024-09-21 23:37:00 -0500

    user_repository

---
+++ pythonProject/app/domain/use_cases/repositories/user_repository.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from domain.model.user import User
+
+class IUserRepository(ABC):
+    @abstractmethod
+    def create(self, user: User) -> User:
+        pass
+    
+    @abstractmethod
+    def read(self, user_id: str) -> Optional[User]:
+        pass
+
+    @abstractmethod
+    def update(self, user_id: str, user: User) -> Optional[User]:
+        pass
+
+    @abstractmethod
+    def delete(self, user_id: str) -> bool:
+        pass
+
+    @abstractmethod
+    def list(self) -> List[User]:
+        pass
