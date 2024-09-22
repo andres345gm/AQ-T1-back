7d24e393c6d045903e763be8c468bd92396ea4e7
andres345gm <andres345gm@gmail.com>
2024-09-21 23:34:42 -0500

    Basic structure

---
+++ pythonProject/app/adapters/in/api.py
@@ -0,0 +1,41 @@
+# adapters/in/api.py
+from fastapi import FastAPI, HTTPException
+from typing import List
+
+from adapters.out.mongo_user_repository import MongoUserRepository
+from domain.use_cases.user_crud_use_case import UserCrudUseCase
+from domain.model.user import User
+
+app = FastAPI()
+
+# Crear el repositorio de MongoDB y los casos de uso
+user_repo = MongoUserRepository("mongodb://localhost:27017")
+user_crud = UserCrudUseCase(user_repo)
+
+@app.post("/user", response_model=User)
+def create_user(username: str, password: str):
+    return user_crud.create_user(username, password)
+
+@app.get("/user/{user_id}", response_model=User)
+def get_user(user_id: str):
+    user = user_crud.get_user(user_id)
+    if user:
+        return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.put("/user/{user_id}", response_model=User)
+def update_user(user_id: str, username: str, password: str):
+    user = user_crud.update_user(user_id, username, password)
+    if user:
+        return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.delete("/user/{user_id}")
+def delete_user(user_id: str):
+    if user_crud.delete_user(user_id):
+        return {"detail": "User deleted"}
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.get("/user", response_model=List[User])
+def list_users():
+    return user_crud.list_users()
---
+++ pythonProject/app/domain/services/pokemon.py
@@ -0,0 +1,7 @@
+class Pokemon:
+    def __init__(self, id: int, name: str, height: int, weight: int, stats: dict):
+        self.id = id
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.stats = stats
